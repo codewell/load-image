e08fc195edc75514c9900f64adb24bd5dbfcfeab
Filip Johansson <filip.johansson@protonmail.com>
2019-11-04 12:01:06 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/**
+   * Loads a url into an js image object.
+   * Resolves the image when the image is loaded.
+   * @param {string} imageUrl - image url
+   * @returns {object} - Returns a promise with the loaded image object
+   */
+const loadImage = imageUrl => new Promise(
+  (resolve, reject) => {
+    const image = new Image();
+    image.onload = () => {
+      resolve(image);
+    }
+    image.onerror = () => {
+      const loadError = new Error(`Failed to load image: \n${imageUrl} \nDo you have a typo in you image url?`);
+      reject(loadError);
+    }
+    image.src = imageUrl;
+  }
+);
